cd79c086b79314298298bd0aa3808c3782d3dab0
yellowb <122517167@qq.com>
2017-11-15 14:46:24 +0800

    [Code] week2-lesson3-problem1

---
+++ code/src/main/java/week2/lesson3/GoodStack.java
@@ -0,0 +1,70 @@
+package week2.lesson3;
+
+/**
+ * 一个逻辑比教程中提供的更完善的Stack
+ */
+public class GoodStack {
+
+    private int[] data;
+    private int size;
+    private int top = 0; // 指向栈的顶部
+
+    public GoodStack(int size) {
+        this.size = size;
+        this.data = new int[size];
+    }
+
+    public void push(int num) {
+        checkUpperBound();
+        data[top++] = num;
+    }
+
+    public int pop() {
+        checkLowerBound();
+        return data[--top];
+    }
+
+    public int getTop() {
+        checkLowerBound();
+        return data[top - 1];
+    }
+
+    public boolean isEmpty() {
+        return top == 0;
+    }
+
+    /**
+     * Clear and reset the top pointer to 0.
+     */
+    public void clear() {
+        top = 0;
+    }
+
+    /**
+     * Check the 'top' pointer is pointed within the valid range.
+     * @throws ArrayIndexOutOfBoundsException
+     */
+    private void checkUpperBound() throws ArrayIndexOutOfBoundsException {
+        if(this.top >= this.size) {
+            throw new ArrayIndexOutOfBoundsException(this.top);
+        }
+    }
+
+    /**
+     * Check the 'top' pointer is pointed within the valid range.
+     * @throws ArrayIndexOutOfBoundsException
+     */
+    private void checkLowerBound() throws ArrayIndexOutOfBoundsException {
+        if(this.top <= 0) {
+            throw new ArrayIndexOutOfBoundsException(this.top - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        GoodStack stack = new GoodStack(3);
+        stack.push(1);
+        stack.push(1);
+        stack.push(1);
+        stack.push(1);
+    }
+}
---
+++ code/src/main/java/week2/lesson3/RevPolishNotationCalculator.java
@@ -0,0 +1,13 @@
+package week2.lesson3;
+
+import week2.lesson1.*;
+
+/**
+ * 后缀表达式计算器
+ */
+public class RevPolishNotationCalculator {
+
+    private GoodStack stack;
+
+
+}
