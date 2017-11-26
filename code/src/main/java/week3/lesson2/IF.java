82aba969c00a515bbb3855e2182e1102bbd0865d
yellowb <122517167@qq.com>
2017-11-26 20:33:08 +0800

    [Code] week3-lesson2

---
+++ code/src/main/java/week3/lesson2/IF.java
@@ -0,0 +1,13 @@
+package week3.lesson2;
+
+public class IF {
+    public static void main(String args[]) {
+        int a = 1;
+        int b = 2;
+        if (a > b) {
+            System.out.println("AAAAAAAAAAA");
+        } else {
+            System.out.println("BBBBBBBBBBB");
+        }
+    }
+}
---
+++ code/src/main/java/week3/lesson2/WHILE.java
@@ -0,0 +1,13 @@
+package week3.lesson2;
+
+public class WHILE {
+    public static void main(String args[]) {
+        int a = 1;
+
+        while (a < 10) {
+            System.out.println("AAAAAAAAAAA");
+            a++;
+        }
+
+    }
+}
