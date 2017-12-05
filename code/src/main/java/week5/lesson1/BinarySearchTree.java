8508a73884b36e5bd9f0918962c54758dc7b0d14
yellowb <122517167@qq.com>
2017-12-05 09:26:33 +0800

    [Code] week5-lesson1

--- code/src/main/java/week5/lesson1/BinarySearchTree.java
+++ code/src/main/java/week5/lesson1/BinarySearchTree.java
@@ -60,25 +60,72 @@ public class BinarySearchTree<T extends Comparable<T>> {
             if (i.compareTo(current.data) == 0) {
                 found = true;
                 break;
-            }
-            else if(i.compareTo(current.data) < 0) {
-                if(current.left != null) {
+            } else if (i.compareTo(current.data) < 0) {
+                if (current.left != null) {
                     current = current.left;
-                }
-                else {
+                } else {
                     break;
                 }
-            }
-            else {
-                if(current.right != null) {
+            } else {
+                if (current.right != null) {
                     current = current.right;
-                }
-                else {
+                } else {
                     break;
                 }
             }
         }
         return found;
+    }
+
+    /**
+     * Pre-Order go through BST
+     */
+    public void preOrderWithRecursive() {
+        preOrderWithRecursive(root);
+    }
+
+    public void preOrderWithRecursive(Node<T> current) {
+        if (current == null) {
+            return;
+        } else {
+            System.out.println(current.data);
+            preOrderWithRecursive(current.left);
+            preOrderWithRecursive(current.right);
+        }
+    }
+
+    /**
+     * Mid-Order go through BST
+     */
+    public void midOrderWithRecursive() {
+        midOrderWithRecursive(root);
+    }
+
+    public void midOrderWithRecursive(Node<T> current) {
+        if (current == null) {
+            return;
+        } else {
+            midOrderWithRecursive(current.left);
+            System.out.println(current.data);
+            midOrderWithRecursive(current.right);
+        }
+    }
+
+    /**
+     * Post-Order go through BST
+     */
+    public void postOrderWithRecursive() {
+        postOrderWithRecursive(root);
+    }
+
+    public void postOrderWithRecursive(Node<T> current) {
+        if (current == null) {
+            return;
+        } else {
+            postOrderWithRecursive(current.left);
+            postOrderWithRecursive(current.right);
+            System.out.println(current.data);
+        }
     }
 
     public static void main(String[] args) {
@@ -88,8 +135,10 @@ public class BinarySearchTree<T extends Comparable<T>> {
         bst.insert(1);
         bst.insert(3);
         bst.insert(2);
+        bst.insert(4);
 
-        System.out.println(bst.contains(3));
+//        System.out.println(bst.contains(3));
+        bst.postOrderWithRecursive();
     }
 
 }
