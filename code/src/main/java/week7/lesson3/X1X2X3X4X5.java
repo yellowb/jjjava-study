c5ee3202a98f1a35627ba47e296b3036885198cb
yellowb <122517167@qq.com>
2017-12-08 10:27:06 +0800

    [Code] week7-lesson3

--- code/src/main/java/week7/lesson3/X1X2X3X4X5.java
+++ code/src/main/java/week7/lesson3/X1X2X3X4X5.java
@@ -10,7 +10,28 @@ import java.util.Map;
  */
 public class X1X2X3X4X5 {
 
-    public int findAnswer(int a1, int a2, int a3, int a4, int a5) {
+    public int findAnswerBad(int a1, int a2, int a3, int a4, int a5) {
+        int amount = 0;
+
+        for(int i1 = -50; i1 <= 50; i1++) {
+            for (int i2 = -50; i2 <= 50; i2++) {
+                for (int i3 = -50; i3 <= 50; i3++) {
+                    for (int i4 = -50; i4 <= 50; i4++) {
+                        for (int i5 = -50; i5 <= 50; i5++) {
+                            int result = a1 * i1 * i1 * i1 + a2 * i2 * i2 * i2 + a3 * i3 * i3 * i3 + a4 * i4 * i4 * i4 + a5 * i5 * i5 * i5;
+                            if(result == 0) {
+                                amount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return amount;
+    }
+
+    public int findAnswerGood(int a1, int a2, int a3, int a4, int a5) {
         int amount = 0;
         Map<Integer, Pair> map = new HashMap<>();
 
@@ -72,7 +93,18 @@ public class X1X2X3X4X5 {
         int a4 = 4;
         int a5 = 5;
 
-        int amount = new X1X2X3X4X5().findAnswer(a1, a2, a3, a4, a5);
-        System.out.println("There are " + amount + " answers.");
+        long start = System.currentTimeMillis();
+        int amount = new X1X2X3X4X5().findAnswerGood(a1, a2, a3, a4, a5);
+        System.out.println("[Good]There are " + amount + " answers.[" + (System.currentTimeMillis() - start) + " ms]");
+        start = System.currentTimeMillis();
+        amount = new X1X2X3X4X5().findAnswerBad(a1, a2, a3, a4, a5);
+        System.out.println("[Bad]There are " + amount + " answers.[" + (System.currentTimeMillis() - start) + " ms]");
+
+        /***
+         *
+         * [Good]There are 17235 answers.[67 ms]
+         * [Bad]There are 17235 answers.[47218 ms]
+         *
+         */
     }
 }
