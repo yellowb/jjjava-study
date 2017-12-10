53b40ba99815e497712c678eb48f096d5f9e05e5
yellowb <122517167@qq.com>
2017-12-11 00:20:50 +0800

    Add All Rangement sample

--- code/src/main/java/allrangement/AllRangement.java
+++ code/src/main/java/allrangement/AllRangement.java
@@ -1,21 +1,50 @@
 package allrangement;
 
+import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 全排列
+ */
 public class AllRangement {
 
     public List<String> allRangement(char[] tokens) {
-        //TODO
-        return null;
+
+        List<String> rangements = new LinkedList<>();
+        allRangement(tokens, 0, tokens.length, rangements);
+
+        return rangements;
     }
 
-    public List<String> allRangement(char[] tokens, int start, int end) {
-        // TODO
-        return null;
+    public void allRangement(char[] tokens, int start, int end, List<String> rangements) {
+        if (start == end - 1) {
+            rangements.add(new String(tokens));
+        } else {
+            for (int i = start; i < end; i++) {
+                swap(tokens, start, i);
+                allRangement(tokens, start + 1, end, rangements);
+                swap(tokens, start, i);
+            }
+        }
+    }
+
+    /**
+     * Swap the char at index i & j
+     *
+     * @param tokens
+     * @param i
+     * @param j
+     */
+    public void swap(char[] tokens, int i, int j) {
+        char temp = tokens[i];
+        tokens[i] = tokens[j];
+        tokens[j] = temp;
     }
 
     public static void main(String[] args) {
-
+        char[] tokens = "abc".toCharArray();
+        List<String> rangements = new AllRangement().allRangement(tokens);
+        System.out.println(rangements);
     }
 
 }
