98c2c41f21dfa5db79e57c55fc1a85e36f590b1c
yellowb <122517167@qq.com>
2017-12-10 23:38:55 +0800

    Add All Rangement sample

---
+++ code/src/main/java/allrangement/AllRangement.java
@@ -0,0 +1,21 @@
+package allrangement;
+
+import java.util.List;
+
+public class AllRangement {
+
+    public List<String> allRangement(char[] tokens) {
+        //TODO
+        return null;
+    }
+
+    public List<String> allRangement(char[] tokens, int start, int end) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
