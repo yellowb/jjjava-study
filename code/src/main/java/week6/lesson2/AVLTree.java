7d4e731e3402d08474bd1d8db70969ad705caac7
yellowb <122517167@qq.com>
2018-01-08 23:24:11 +0800

    [week8]

--- code/src/main/java/week6/lesson2/AVLTree.java
+++ code/src/main/java/week6/lesson2/AVLTree.java
@@ -229,7 +229,7 @@ public class AVLTree<T extends Comparable<T>> {
         AVLTree<Integer> avlTree = new AVLTree<>();
         avlTree.insert(5);
         avlTree.insert(3);
-        avlTree.insert(6);
+        avlTree.insert(6); 
         avlTree.insert(2);
         avlTree.insert(4);
         avlTree.insert(1);
